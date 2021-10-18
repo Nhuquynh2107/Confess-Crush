77e6d69a2f72b3254a883aa80852f5f3f3402a95
Nhuquynh2107 <92729311+Nhuquynh2107@users.noreply.github.com>
2021-10-18 21:22:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ const CONFIG = {
     introTitle: 'Bà xã ahhh',
     introDesc: `Chúc mừng chuyện tình mình được thêm một tháng nữa haha, nhưng mà như thé thì hong được đâu, phải đi nhiều hơn cơ.
     Dự định là sẽ đón anni cùng em, nhưng hoàn cảnh hong cho phép mất ròiiiii, giá như nhậu hôm nay thì đã được ở cùng nhau rồi vợ nhờ. 
-    Ox chưa hứa với em gì ngoài việc sẽ cưới em, nhưng mà ox hứa là sẽ đón thật nhiều cái anni cùng bé nữaaa cho nên là bbi của ox đừng buồn một mình nhaaa, ngày trọng đại như này phải vui chứ hong được khóc đâu nha ^^
+    Ox chưa hứa với em gì ngoài việc sẽ cưới em, nhưng mà ox hứa là sẽ đón thật nhiều cái anni cùng bé nữaaa, cho nên là bbi của ox đừng buồn một mình nhaaa, ngày trọng đại như này phải vui chứ hong được khóc đâu nha ^^
     Cám ơn bé vì đã cho ox cơ hội chăm sóc, che chở em. Dù cho có là hôm nay hay những ngày sau nữa, thì ox vẫn muốn nói là ox thương em, thương em bé của ox rất nhiều. 
-    Happy anni babi, gửi ngàn nụ hôn tới em, moahh, 
+    Happy anni babi, gửi ngàn nụ hôn tới em, moahh`, 
     btnIntro: 'Bbi ấn vào điii',
     title: 'Happy anni 3 tháng của tụi mìnhhhh 🥰',
     desc: 'Bé có thương ox hemmmm ',
@@ -16,6 +16,6 @@ const CONFIG = {
     reply: 'Dạ có',
     mess: 'Ox thương em nhất trên đời ',
     messDesc: 'Mình đi với nhau thật lâu thật lâu nhó bbiiii',
-    btnAccept: 'Dạaaaaa',
+    btnAccept: 'Cái này bbi hong trả lời được nên ox trả lời dùm cho đó nha, đương nhiên là phải chịu òi haha',
     messLink: 'https://www.facebook.com/messages/t/100040389039681' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
